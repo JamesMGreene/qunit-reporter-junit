6d0469d273b350149b677c24762338696c64834d
James M. Greene <james.m.greene@gmail.com>
2016-10-03 02:32:21 -0500

    Updates to support the latest versions of QUnit and Grunt

--- test/qunit-reporter-junit.test.js
+++ test/qunit-reporter-junit.test.js
@@ -1,32 +1,43 @@
-QUnit.jUnitReport = function(data) {
-	var console = window.console;
-	if (console) {
-		console.log(data.xml);
-	}
-};
+(function( module, test ) {
 
-QUnit.module('Module 1');
+	QUnit.jUnitReport = function(data) {
+		var console = window.console;
+		if (console) {
+			console.log(data.xml);
+		}
+	};
 
-QUnit.test('test 1', 2, function (assert) {
-	assert.equal(1, 1, 'Assert 1 = 1');
-	assert.equal(1, 2, 'Assert fail 1 = 2');
-});
 
-QUnit.test('test 2', 3, function (assert) {
-	assert.equal(1, 1, 'Assert 1 = 1');
-	assert.equal(1, 2, 'Assert fail 1 = 2');
-	assert.equal(1, 1, 'Assert 1 = 1');
-});
+	module('Module 1');
 
-QUnit.module('Module 2');
+	test('test 1', function(assert) {
+		assert.expect(2);
+		assert.equal(1, 1, 'Assert 1 = 1');
+		assert.equal(1, 2, 'Assert fail 1 = 2');
+	});
 
-QUnit.test('test 3', 2, function (assert) {
-	assert.equal(1, 1, 'Assert 1 = 1');
-	assert.equal(1, 2, 'Assert fail 1 = 2');
-});
+	test('test 2', function(assert) {
+		assert.expect(3);
+		assert.equal(1, 1, 'Assert 1 = 1');
+		assert.equal(1, 2, 'Assert fail 1 = 2');
+		assert.equal(1, 1, 'Assert 1 = 1');
+	});
 
-QUnit.test('test 4', 3, function (assert) {
-	assert.equal(1, 1, 'Assert 1 = 1');
-	assert.equal(1, 2, 'Assert fail 1 = 2');
-	assert.equal(1, 3, 'Assert fail 1 = 3');
-});
+
+	module('Module 2');
+
+	test('test 3', function(assert) {
+		assert.expect(2);
+		assert.equal(1, 1, 'Assert 1 = 1');
+		assert.equal(1, 2, 'Assert fail 1 = 2');
+	});
+
+	test('test 4', function(assert) {
+		assert.expect(3);
+		assert.equal(1, 1, 'Assert 1 = 1');
+		assert.equal(1, 2, 'Assert fail 1 = 2');
+		assert.equal(1, 3, 'Assert fail 1 = 3');
+	});
+
+
+})( QUnit.module, QUnit.test );
